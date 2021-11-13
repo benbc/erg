ed15e70657168e7db720f70d277b407467443eb7
Ben Butler-Cole <ben@bridesmere.com>
2021-11-13 11:11:06 +0000

    Inject the technique into the model

--- erg.py
+++ erg.py
@@ -4,9 +4,9 @@ from tabulate import tabulate
 from lib import vector, dot, sum_vector, hadamard, hadamard_inv, solve, only, vector_to_tuple, round_all
 
 
-def run(omega):
+def run(technique, omega):
     """
-    Solve the model for a given wage rate (omega).
+    Solve the model for a given wage rate (omega) and technique.
     """
     # Free variables
     #
@@ -16,11 +16,9 @@ def run(omega):
     #   - scalars are greek lowercase or indexed roman lowercase where they appear as vector components
     p1, p3, rho, w1, w2, k0, k1, k2, k3, kappa = symbols('p1 p3 rho w1 w2 k0 k1 k2 k3 kappa', negative=False, real=True)
 
-    # Process definition. These are the numerical inputs to the model along with omega.
-    A = Matrix([[0.3, 0.2, 0], [0, 0.4, 0.2], [1, 0, 0]])  # Inputs.
-    I = eye(3)
-    assert (I - A).det() >= 0, "Production matrix is not productive"  # Kurz and Salvadori eq 4.4
-    l = vector(1, 0.25, 0.6)  # Labour required.
+    # Production matrix and labour vector
+    A, l = technique
+    assert (eye(3) - A).det() >= 0, "Production matrix is not productive"  # Kurz and Salvadori eq 4.4
 
     p = vector(p1, 1, p3)  # Prices.
     w = vector(w1, w1, 0)  # Wage basket. Composition isn't fully determined: pick one where the amounts are equal.
@@ -58,10 +56,14 @@ def main():
     table = []
     headers = ["wage rate", "prices", "profit", "wages", "assignments", "unassigned"]
 
+    A = Matrix([[0.3, 0.2, 0], [0, 0.4, 0.2], [1, 0, 0]])
+    l = vector(1, 0.25, 0.6)
+    technique = (A, l)
+
     # These are almost Hahnel's examples. He's rounding his numbers, though, and his 0.691 actually gives a tiny
     # negative number which our model rejects.
     for o in [0.6909, 0.5, 0.4]:
-        p, rho, w, k, k0 = run(o)
+        p, rho, w, k, k0 = run(technique, o)
         table.append(round_all([o, vector_to_tuple(p), rho, vector_to_tuple(w), vector_to_tuple(k), k0]))
 
     print(tabulate(table, headers=headers))
